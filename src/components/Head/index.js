5309b55d46e9a692ad5a0dd2fe65b6fe77673125
GabyFutemma <gaby.futemma@gmail.com>
2021-01-27 20:23:49 -0300

    teste metatag

--- src/components/Head/index.js
+++ src/components/Head/index.js
@@ -1,9 +1,5 @@
-import Head from 'next/head'
-import { useRouter } from 'next/router'
-
-function currentPath(){
-  return window.location.href
-}
+import Head from 'next/head';
+const path = "https://github.com/gabyfutemma/poefanquiz/blob/main/public/";
 
 function IndexPage({title, imgBg, favicon, description}) {
   return (
@@ -17,13 +13,13 @@ function IndexPage({title, imgBg, favicon, description}) {
         <meta property="og:url" content="https://poefanquiz.gabyfutemma.vercel.app/" />
         <meta property="og:title" content={title} />
         <meta property="og:description" content={description} />
-        <meta property="og:image" content={currentPath() + imgBg}/>
+        <meta property="og:image" content={path + imgBg}/>
 
         <meta property="twitter:card" content="summary_large_image" />
         <meta property="twitter:url" content="https://poefanquiz.gabyfutemma.vercel.app/" />
         <meta property="twitter:title" content={title} />
         <meta property="twitter:description" content={description} />
-        <meta property="twitter:image" content={currentPath() + imgBg} />
+        <meta property="twitter:image" content={path + imgBg} />
       </Head>
     </div>
   )
