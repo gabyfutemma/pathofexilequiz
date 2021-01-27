b046f66a2e42e53519f610b26c40672cc5713303
GabyFutemma <gaby.futemma@gmail.com>
2021-01-27 19:42:17 -0300

    teste metatag

--- src/components/Head/index.js
+++ src/components/Head/index.js
@@ -1,12 +1,29 @@
 import Head from 'next/head'
+import { useRouter } from 'next/router'
 
-function IndexPage({title, imgBg, favicon}) {
+function currentPath(){
+  return window.location.href
+}
+
+function IndexPage({title, imgBg, favicon, description}) {
   return (
     <div>
       <Head>
         <title>{title}</title>
-        <meta property="og:image" content={imgBg}/>
         <link rel="icon" type="image/png" href={favicon}/>
+        <meta name="description" content={description}/>
+
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://poefanquiz.gabyfutemma.vercel.app/" />
+        <meta property="og:title" content={title} />
+        <meta property="og:description" content={description} />
+        <meta property="og:image" content={currentPath() + imgBg}/>
+
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta property="twitter:url" content="https://poefanquiz.gabyfutemma.vercel.app/" />
+        <meta property="twitter:title" content={title} />
+        <meta property="twitter:description" content={description} />
+        <meta property="twitter:image" content={currentPath() + imgBg} />
       </Head>
     </div>
   )
